8423447b9d5163c0468aee5675afdfec7a74be81
David Andres Gallego Mesa <davidgallego0125@gmail.com>
2016-06-18 15:14:00 -0400

    Ajout de la branche pour la tâche #1 et ajout de la classe RegionFiller

---
+++ src/controller/RegionFiller.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * Created by david on 6/18/2016.
+ */
+public class RegionFiller {
+    /**
+     * Constructeur par défaut.
+     * **/
+    public RegionFiller(){}
+}
